f8443ef3d8709ed775fe2c8d8f256c0b01766c8d
Sergey Korolev <humankind@yandex.ru>
2014-11-18 15:55:37 +0300

    Шаблон паттерна Class Adapter

---
+++ Pattern ClassAdapter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pattern_ClassAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITargetInterface r;
+            r = new ClassAdapter();
+            r.targetMethod();
+        }
+    }
+
+    interface ITargetInterface
+    {
+        void targetMethod();
+    }
+
+    class Adaptee
+    {
+        protected void Method() // Закрыт на уровне объекта
+        {
+            Console.WriteLine("Работает наш метод.");
+            Console.ReadLine();
+        }
+    }
+
+    class ClassAdapter: Adaptee, ITargetInterface
+    {
+        public void targetMethod()
+        {
+            Method();
+        }
+    }
+}
